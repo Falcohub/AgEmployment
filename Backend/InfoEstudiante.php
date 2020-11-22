0ddf5d6e019952af8d933f83f9e8803ac60743da
Obeimar Sanchez <osaamc@gmail.com>
2020-11-22 18:54:36 -0500

    cargar archivos en info estudiante

--- Backend/InfoEstudiante.php
+++ Backend/InfoEstudiante.php
@@ -1,6 +1,45 @@
 <?php
     include_once 'conexion.php';
 
+    $directorio = "Documentos/";
+
+    $archivo = $directorio . basename($_FILES["file"]["name"]);
+
+    $tipoArchivo = pathinfo($archivo, PATHINFO_EXTENSION);
+
+      // valida que es imagen
+     //$checarSiImagen = file_exists($_FILES["file"]["tmp_name"]);
+
+    if($tipoArchivo != false){
+
+        //validando tamaño del archivo
+        $size = $_FILES["file"]["size"];
+
+        if($size > 900000){
+            echo "El archivo tiene que ser menor a 900kb";
+        }else{
+
+            //validar tipo de imagen
+            if($tipoArchivo == "docx" || $tipoArchivo == "pdf" || $tipoArchivo == "doc"){
+                // se validó el archivo correctamente
+                if(move_uploaded_file($_FILES["file"]["tmp_name"], $archivo)){
+                     echo '
+                    <script>
+                        alert("Datos guardados correctamente.");
+                        window.location = "../frontend/Frm_Infoestudiante.php";
+                    </script>
+                    ';
+                }else{
+                    echo "Hubo un error en la subida del archivo";
+                }
+            }else{
+                echo "Solo se admiten archivos docx/pdf";
+            }
+        }
+    }else{
+        echo "No es un Documento";
+    }
+
     session_start();
     
     //Establecer conexión
@@ -24,7 +63,7 @@
     if (isset($_SESSION['idRegistroEst'])) {       
 
         //----- Actualizar datos para completar registro
-        $ActualizarEstudiante = $conexion->prepare("UPDATE tbl_usuarios SET user_pkid = '$documento', user_ddi = '$ddi', user_nombres = '$nombres', user_apellidos = '$apellidos', user_sexo = '$sexo', user_contacto = '$contacto', user_correo = '$correo', user_dpto = '$dpto', user_ciudad = '$ciudad', user_direccion = '$direccion', user_fechaNac = '$fechaNac', user_perfil = '$perfil' WHERE user_pkid = {$_SESSION['idRegistroEst']}");
+        $ActualizarEstudiante = $conexion->prepare("UPDATE tbl_usuarios SET user_pkid = '$documento', user_ddi = '$ddi', user_nombres = '$nombres', user_apellidos = '$apellidos', user_sexo = '$sexo', user_contacto = '$contacto', user_correo = '$correo', user_dpto = '$dpto', user_ciudad = '$ciudad', user_direccion = '$direccion', user_fechaNac = '$fechaNac', user_perfil = '$perfil' , Hoja = '$checarSiImagen'  WHERE user_pkid = {$_SESSION['idRegistroEst']}");
 
             if ($ActualizarEstudiante->execute()) {
                 $_SESSION['idRegistroEst'] = $documento;
@@ -38,7 +77,7 @@
     }else if (isset($_SESSION['idLogin'])) {
     
         //----- Actualizar datos para completar registro
-        $ActualizarEstudiante = $conexion->prepare("UPDATE tbl_usuarios SET user_pkid = '$documento', user_ddi = '$ddi', user_nombres = '$nombres', user_apellidos = '$apellidos', user_sexo = '$sexo', user_contacto = '$contacto', user_correo = '$correo', user_dpto = '$dpto', user_ciudad = '$ciudad', user_direccion = '$direccion', user_fechaNac = '$fechaNac', user_perfil = '$perfil' WHERE user_pkid = {$_SESSION['idLogin']}");
+        $ActualizarEstudiante = $conexion->prepare("UPDATE tbl_usuarios SET user_pkid = '$documento', user_ddi = '$ddi', user_nombres = '$nombres', user_apellidos = '$apellidos', user_sexo = '$sexo', user_contacto = '$contacto', user_correo = '$correo', user_dpto = '$dpto', user_ciudad = '$ciudad', user_direccion = '$direccion', user_fechaNac = '$fechaNac', user_perfil = '$perfil', Hoja = '$checarSiImagen'  WHERE user_pkid = {$_SESSION['idLogin']}");
     
             if ($ActualizarEstudiante->execute()) {
                 $_SESSION['idLogin'] = $documento;
